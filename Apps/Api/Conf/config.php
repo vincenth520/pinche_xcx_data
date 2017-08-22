3e82e74f66af20616651613c472b7adcdfa3e52d
vincenth520 <1091986039@qq.com>
2017-08-22 14:31:51 +0800

    修复无法加载缓存类型错误

--- Apps/Api/Conf/config.php
+++ Apps/Api/Conf/config.php
@@ -1,6 +1,6 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
-	'APPID' => 'wx0aa456241abc9e8e',
-	'AppSecret' => '58ec56f0d3f37e4dcee7a75220bc6766',
+	'APPID' => 'wx61f7708fbb157848',
+	'AppSecret' => 'f860690de9733e931b4cf030bf758a71',
 );
